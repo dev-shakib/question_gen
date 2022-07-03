1142332b1a2265205476327ca6332c32244e9310
mehedi-lskit <106152285+mehedi-lskit@users.noreply.github.com>
2022-07-03 12:08:43 +0600

    Add files via upload
    new design

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+// responsive menu 
+var menu_bar = document.getElementById("menu-bar");
+var menu_sec = document.getElementById("menu");
+
+menu_bar.addEventListener("click", function(){
+    menu_sec.classList.toggle("block");
+});
+
+// slider 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+//sticky header
+window.addEventListener("scroll", function() {
+  const header = document.getElementById("header"),
+        scroll = window.pageYOffset | document.body.scrollTop;
+
+  if (scroll > 300) { header.className = "header-sec sticky"; }
+  else if (scroll <= 300) { header.className = "header-sec"; }
+});
